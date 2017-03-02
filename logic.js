68b385101be3011f15e5db675b4818ff9e84b5bb
Sophia Dossland <sophia.dossland@gmail.com>
2017-03-02 14:42:11 -0800

    flex panel movement commit

--- logic.js
+++ logic.js
@@ -1,10 +1,16 @@
 /**
  * Created by sophia on 3/1/17.
  */
-document.addEventListener("DOMLoadedContent", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
   const panels = document.querySelectorAll('.panel');
 
-  panels.forEach()
+  function toggleOpen() {
+    this.classList.toggle('open');
+    this.classList.toggle('openActive');
+    this.classList.toggle('openText');
+  }
+
+  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
 });
