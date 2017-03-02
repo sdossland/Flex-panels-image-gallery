afe18dfe4257e4cead965f85839263b1bd7af739
Sophia Dossland <sophia.dossland@gmail.com>
2017-03-02 13:52:44 -0800

    panel images

--- logic.js
+++ logic.js
@@ -1,3 +1,10 @@
 /**
  * Created by sophia on 3/1/17.
  */
+document.addEventListener("DOMLoadedContent", function () {
+
+  const panels = document.querySelectorAll('.panel');
+
+  panels.forEach()
+
+});
